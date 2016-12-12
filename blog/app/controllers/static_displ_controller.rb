b99b4f2fba9f3c8059bbd00139a33595e7078233
PetriJ123 <mr.pepe999@gmail.com>
2016-12-12 21:28:05 +0100

    Added more items to the menu-list

--- blog/app/controllers/static_displ_controller.rb
+++ blog/app/controllers/static_displ_controller.rb
@@ -1,10 +1,17 @@
 class StaticDisplController < ApplicationController
-  def landing
-      # Empty controller-method
-  end
+    def landing
+        # Empty controller-method
+    end
 
-  def about
-      # Empty controller-method
-  end
-    
+    def project
+        # Empty controller-method 
+    end
+
+    def countries
+        # Empty controller-method
+    end
+
+    def about
+        # Empty controller-method
+    end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     root 'static_displ#landing'
 
     get 'static_displ/about'
+    
+    get 'static_displ/projects'
+    
+    get 'static_displ/countries'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
