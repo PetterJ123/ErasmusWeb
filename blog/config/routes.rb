4827360e89a3eadfbd9e7f6f59a97872dc4c03b2
PetterJ123 <mr.pepe999@gmail.com>
2017-01-17 22:23:21 +0100

    Null update

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-    get 'static_displ/landing'
-    root 'static_displ#landing'
-
-    get 'static_displ/about'
-    
-    get 'static_displ/projects'
-    
-    get 'static_displ/countries'
-    
-    get 'static_displ/blog'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
