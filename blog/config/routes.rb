2413bb1133f9f680b20caff426751623975f4ee2
PetterJ123 <mr.pepe999@gmail.com>
2017-02-18 13:46:54 +0100

    Fixed error with user not being defined

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
     get 'menu/about'
 
+    get 'menu/profile'
+
     devise_for :users
     resources :posts
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
